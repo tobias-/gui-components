4f4b1e7b4292459ea3df09cf27ea9e43a459609d
Tobias <tobias@olsson.be>
2013-03-02 19:33:52 +0100

    Removed superfluous keywords
    Change-Id: I91382745a21dc4815121be655ffd37b4a5a6e810

--- src/main/java/com/sourceforgery/guicomponents/config/AvailableConfigParameters.java
+++ src/main/java/com/sourceforgery/guicomponents/config/AvailableConfigParameters.java
@@ -1,11 +1,10 @@
 package com.sourceforgery.guicomponents.config;
 
 public interface AvailableConfigParameters {
-	public String name();
-	public String getParam();
-	public Class<?> getClazz();
-	public Boolean getBoolean();
-	public Integer intValue();
-	public<T> T get();
-
+	String name();
+	String getParam();
+	Class<?> getClazz();
+	Boolean getBoolean();
+	Integer intValue();
+	<T> T get();
 }
--- src/main/java/nongui/Filter.java
+++ src/main/java/nongui/Filter.java
@@ -1,10 +1,10 @@
 package nongui;
 
 public interface Filter<T> {
-	public boolean isVisible(T object);
+	boolean isVisible(T object);
 
 	@SuppressWarnings("rawtypes")
-	public static final Filter ALL_VISIBLE = new Filter() {
+	static final Filter ALL_VISIBLE = new Filter() {
 		@Override
 		public boolean isVisible(final Object object) {
 			return true;
