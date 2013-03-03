7a9c28f0da0ecdbd5beb3318949dfa5589eb6c70
Tobias <tobias@olsson.be>
2013-03-03 03:14:50 +0100

    Moved AlphanumericalSorter to guicomponent
    Change-Id: I5d7cd53f23566f92a1ed5f182f95d29fcc3f8788

---
+++ src/main/java/nongui/AlphaNumericalSorter.java
@@ -0,0 +1,67 @@
+package nongui;
+
+import static java.lang.Character.isDigit;
+import static java.lang.Character.toUpperCase;
+
+import java.util.Comparator;
+
+public class AlphaNumericalSorter<T> implements Comparator<T> {
+
+	@Override
+	public int compare(final T x1, final T x2) {
+		String o1 = x1.toString();
+		String o2 = x2.toString();
+		int pos1 = 0;
+		int pos2 = 0;
+		int len1 = o1.length();
+		int len2 = o2.length();
+		int diff = 0;
+		while (pos1 < len1 && pos2 < len2 && diff == 0) {
+			char c1 = o1.charAt(pos1);
+			char c2 = o2.charAt(pos2);
+			if (isDigit(c1) && isDigit(c2)) {
+				while(pos1 < len1 && o1.charAt(pos1) == '0' ) {
+					pos1++;
+				}
+				while(pos2 < len2 && o2.charAt(pos2) == '0') {
+					pos2++;
+				}
+				int numLen1 = 0;
+				int numLen2 = 0;
+				int numDiff = 0;
+				while(pos1 < len1 && pos2 < len2 && numDiff == 0 && isDigit(o1.charAt(pos1)) && isDigit(o2.charAt(pos2))) {
+					numDiff = o1.charAt(pos1) - o2.charAt(pos2);
+					numLen1++;
+					numLen2++;
+					pos1++;
+					pos2++;
+				}
+				while(pos1 < len1 && isDigit(o1.charAt(pos1))) {
+					numLen1++;
+					pos1++;
+				}
+				while(pos2 < len2 && isDigit(o2.charAt(pos2))) {
+					numLen2++;
+					pos2++;
+				}
+				if (numLen1 != numLen2) {
+					diff = numLen1 - numLen2;
+				} else {
+					diff = numDiff;
+				}
+			} else {
+				diff = toUpperCase(c1) - toUpperCase(c2);
+				pos1++;
+				pos2++;
+			}
+		}
+		if (diff == 0) {
+			if (pos1 < len1) {
+				diff = 1;
+			} else if (pos2 < len2) {
+				diff = -1;
+			}
+		}
+		return diff;
+	}
+}
---
+++ src/test/java/AlphaNumericalSorterTest.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.TreeSet;
+
+import nongui.AlphaNumericalSorter;
+
+import org.junit.Test;
+
+public class AlphaNumericalSorterTest {
+	private final AlphaNumericalSorter<String> sort = new AlphaNumericalSorter<String>();
+
+	private void sortAndTestData(final String[] data, final String[] result) {
+		TreeSet<String> ts = new TreeSet<String>(new AlphaNumericalSorter<String>());
+		for (String a : data) {
+			ts.add(a);
+		}
+		String[] rs2 = ts.toArray(new String[0]);
+		String join1 = "";
+		String join2 = "";
+		for (int i = 0; i < result.length; i++) {
+			if (result[i] != rs2[i]) {
+				/*
+				 * fail("At array position: " + i + " expected:<" + result[i]
+				 * + "> but was:<" + rs2[i] + ">");
+				 */
+				join1 = join1 + result[i] + "\n";
+				join2 = join2 + rs2[i] + "\n";
+			}
+		}
+		assertEquals("Failed comparison", join1, join2);
+	}
+
+	@Test
+	public void testAlphaNumerical() {
+		String[] data = { "a10b", "a001c", "a0", "a01", "a01d", "a1b", "a010a" };
+		String[] result = { "a0", "a01", "a1b", "a001c", "a01d", "a010a", "a10b" };
+		sortAndTestData(data, result);
+	}
+
+	@Test
+	public void testAlphabetical() {
+		String[] data = { "ooC8l", "uu4Ga", "eiHe6", "aFee2", "aNa9y", "Pae1x" };
+		String[] result = { "aFee2", "aNa9y", "eiHe6", "ooC8l", "Pae1x", "uu4Ga" };
+		sortAndTestData(data, result);
+	}
+
+	@Test
+	public void testEmptyStrings() {
+		assertTrue(sort.compare("", "a") < 0);
+		assertTrue(sort.compare("", "1") < 0);
+		assertTrue(sort.compare("", "") == 0);
+		assertTrue(sort.compare("a", "") > 0);
+		assertTrue(sort.compare("1", "") > 0);
+	}
+
+	@Test
+	public void testStringCompare() {
+		assertTrue(sort.compare("aas", "aas") == 0);
+	}
+
+	@Test
+	public void testNumberCompare() {
+		assertTrue(sort.compare("123.01233", "0123.1233") == 0);
+		assertTrue(sort.compare("123a012aa33", "0123.12a33") > 0);
+	}
+}
