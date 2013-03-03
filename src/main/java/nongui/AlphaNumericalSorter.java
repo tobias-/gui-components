d413f71c81f16e4fd79deeeb21fc08e3d01df6bf
Tobias <tobias@olsson.be>
2013-03-03 21:53:45 +0100

    Yet another attempt to reduce cyclic dep
    This unfortunately creates two objects(MutableInt) for every comparation
    
    Change-Id: I8777c0e5bc02e274cbfdd0dc5fbec78e0adeff13

--- src/main/java/nongui/AlphaNumericalSorter.java
+++ src/main/java/nongui/AlphaNumericalSorter.java
@@ -6,52 +6,51 @@ import static java.lang.Character.toUpperCase;
 import java.io.Serializable;
 import java.util.Comparator;
 
+import org.apache.commons.lang3.mutable.MutableInt;
+
 public class AlphaNumericalSorter<T> implements Comparator<T>, Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private int skipZeros(final int origPos, final String o) {
-		int pos = origPos;
+	private void skipZeros(final MutableInt pos, final String o) {
 		int len = o.length();
-		while(pos < len && o.charAt(pos) == '0' ) {
-			pos++;
+		while(pos.intValue() < len && o.charAt(pos.intValue()) == '0' ) {
+			pos.increment();
 		}
-		return pos;
 	}
 
+	private int countDigits(final String str, final MutableInt pos) {
+		int numLen = 0;
+		int len = str.length();
+		while(pos.intValue() < len && isDigit(str.charAt(pos.intValue()))) {
+			numLen++;
+			pos.increment();
+		}
+		return numLen;
+	}
 
 	@Override
 	public int compare(final T x1, final T x2) {
 		String o1 = x1.toString();
 		String o2 = x2.toString();
-		int pos1 = 0;
-		int pos2 = 0;
+		MutableInt pos1 = new MutableInt();
+		MutableInt pos2 = new MutableInt();
 		int len1 = o1.length();
 		int len2 = o2.length();
 		int diff = 0;
-		while (pos1 < len1 && pos2 < len2 && diff == 0) {
-			char c1 = o1.charAt(pos1);
-			char c2 = o2.charAt(pos2);
+		while (pos1.intValue() < len1 && pos2.intValue() < len2 && diff == 0) {
+			char c1 = o1.charAt(pos1.intValue());
+			char c2 = o2.charAt(pos2.intValue());
 			if (isDigit(c1) && isDigit(c2)) {
-				pos1 = skipZeros(pos1, o1);
-				pos2 = skipZeros(pos2, o2);
-				int numLen1 = 0;
-				int numLen2 = 0;
+				skipZeros(pos1, o1);
+				skipZeros(pos2, o2);
 				int numDiff = 0;
-				while(pos1 < len1 && pos2 < len2 && numDiff == 0 && isDigit(o1.charAt(pos1)) && isDigit(o2.charAt(pos2))) {
-					numDiff = o1.charAt(pos1) - o2.charAt(pos2);
-					numLen1++;
-					numLen2++;
-					pos1++;
-					pos2++;
+				while(pos1.intValue() < len1 && pos2.intValue() < len2 && numDiff == 0 && isDigit(o1.charAt(pos1.intValue())) && isDigit(o2.charAt(pos2.intValue()))) {
+					numDiff = o1.charAt(pos1.intValue()) - o2.charAt(pos2.intValue());
+					pos1.increment();
+					pos2.increment();
 				}
-				while(pos1 < len1 && isDigit(o1.charAt(pos1))) {
-					numLen1++;
-					pos1++;
-				}
-				while(pos2 < len2 && isDigit(o2.charAt(pos2))) {
-					numLen2++;
-					pos2++;
-				}
+				int numLen1 = countDigits(o1, pos1);
+				int numLen2 = countDigits(o2, pos2);
 				if (numLen1 != numLen2) {
 					diff = numLen1 - numLen2;
 				} else {
@@ -59,14 +58,14 @@ public class AlphaNumericalSorter<T> implements Comparator<T>, Serializable {
 				}
 			} else {
 				diff = toUpperCase(c1) - toUpperCase(c2);
-				pos1++;
-				pos2++;
+				pos1.increment();
+				pos2.increment();
 			}
 		}
 		if (diff == 0) {
-			if (pos1 < len1) {
+			if (pos1.intValue() < len1) {
 				diff = 1;
-			} else if (pos2 < len2) {
+			} else if (pos2.intValue() < len2) {
 				diff = -1;
 			}
 		}
