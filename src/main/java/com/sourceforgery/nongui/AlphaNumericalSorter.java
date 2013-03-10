f51276bd18c7541f6f24a5da5e0e6c3084cf6431
Tobias <tobias@olsson.be>
2013-03-10 23:28:19 +0100

    More complex, but much faster.
    Depends on toString() of the compared objects being very fast or it will
    slow down to a crawl
    
    Change-Id: I01a70a9d0be9868f75b3478b2cb928c8f2a28fb5

--- src/main/java/com/sourceforgery/nongui/AlphaNumericalSorter.java
+++ src/main/java/com/sourceforgery/nongui/AlphaNumericalSorter.java
@@ -6,8 +6,6 @@ import static java.lang.Character.toLowerCase;
 import java.io.Serializable;
 import java.util.Comparator;
 
-import org.apache.commons.lang3.mutable.MutableInt;
-
 public class AlphaNumericalSorter<T> implements Comparator<T>, Serializable {
 	private static final long serialVersionUID = 1L;
 	private final boolean caseInsensitive;
@@ -20,45 +18,47 @@ public class AlphaNumericalSorter<T> implements Comparator<T>, Serializable {
 		this(true);
 	}
 
-	private void skipZeros(final MutableInt pos, final String o) {
-		int len = o.length();
-		while(pos.intValue() < len && o.charAt(pos.intValue()) == '0' ) {
-			pos.increment();
-		}
-	}
-
-	private int countDigits(final String str, final MutableInt pos) {
-		int numLen = 0;
-		int len = str.length();
-		while(pos.intValue() < len && isDigit(str.charAt(pos.intValue()))) {
-			numLen++;
-			pos.increment();
-		}
-		return numLen;
-	}
-
 	@Override
 	public int compare(final T x1, final T x2) {
 		String o1 = x1.toString();
 		String o2 = x2.toString();
-		MutableInt pos1 = new MutableInt();
-		MutableInt pos2 = new MutableInt();
+		int pos1 = 0;
+		int pos2 = 0;
 		int len1 = o1.length();
 		int len2 = o2.length();
 		int diff = 0;
-		while (pos1.intValue() < len1 && pos2.intValue() < len2 && diff == 0) {
-			char c1 = o1.charAt(pos1.intValue());
-			char c2 = o2.charAt(pos2.intValue());
+		while (pos1 < len1 && pos2 < len2 && diff == 0) {
+			char c1 = o1.charAt(pos1);
+			char c2 = o2.charAt(pos2);
 			if (isDigit(c1) && isDigit(c2)) {
-				skipZeros(pos1, o1);
-				skipZeros(pos2, o2);
-				int numDiff = findDifference(o1, o2, pos1, pos2);
-				int numLen1 = countDigits(o1, pos1);
-				int numLen2 = countDigits(o2, pos2);
+				while(pos1 < len1 && o1.charAt(pos1) == '0' ) {
+					pos1++;
+				}
+				while(pos2 < len2 && o2.charAt(pos2) == '0' ) {
+					pos2++;
+				}
+				int digitDiff = 0;
+				int numLen1 = 0;
+				int numLen2 = 0;
+				while(pos1 < len1 && pos2 < len2 && digitDiff == 0 && isDigit(o1.charAt(pos1)) && isDigit(o2.charAt(pos2))) {
+					digitDiff = o1.charAt(pos1) - o2.charAt(pos2);
+					pos1++;
+					pos2++;
+					numLen1++;
+					numLen2++;
+				}
+				while(pos1 < len1 && isDigit(o1.charAt(pos1))) {
+					numLen1++;
+					pos1++;
+				}
+				while(pos2 < len2 && isDigit(o2.charAt(pos2))) {
+					numLen2++;
+					pos2++;
+				}
 				if (numLen1 != numLen2) {
 					diff = numLen1 - numLen2;
 				} else {
-					diff = numDiff;
+					diff = digitDiff;
 				}
 			} else {
 				if (caseInsensitive) {
@@ -66,29 +66,17 @@ public class AlphaNumericalSorter<T> implements Comparator<T>, Serializable {
 				} else {
 					diff = c1 - c2;
 				}
-				pos1.increment();
-				pos2.increment();
+				pos1++;
+				pos2++;
 			}
 		}
 		if (diff == 0) {
-			if (pos1.intValue() < len1) {
+			if (pos1++ < len1) {
 				diff = 1;
-			} else if (pos2.intValue() < len2) {
+			} else if (pos2++ < len2) {
 				diff = -1;
 			}
 		}
 		return diff;
-	}
-
-	private int findDifference(final String o1, final String o2, final MutableInt pos1, final MutableInt pos2) {
-		int digitDiff = 0;
-		int len1 = o1.length();
-		int len2 = o2.length();
-		while(pos1.intValue() < len1 && pos2.intValue() < len2 && digitDiff == 0 && isDigit(o1.charAt(pos1.intValue())) && isDigit(o2.charAt(pos2.intValue()))) {
-			digitDiff = o1.charAt(pos1.intValue()) - o2.charAt(pos2.intValue());
-			pos1.increment();
-			pos2.increment();
-		}
-		return digitDiff;
 	}
 }
